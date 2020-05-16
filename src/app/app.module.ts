88c25c7d0b1e550865161fe43937e0e414562e2f
vinaykannambadi <vinaykannambadi@gmail.com>
2020-05-16 22:42:22 +0530

    product-row-list-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ProductComponent ],
+  declarations: [ AppComponent, HelloComponent, ProductComponent, ProductListComponent, ProductRowComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+
+import {Product} from './product.model';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+products : Product[];
+  constructor() { 
+   this.products = [
+      new Product(
+        'NICEHAT', // sku
+        'A Nice Black Hat', // name
+        '/assets/images/products/black-hat.jpg', // imageUrl
+        ['Men', 'Accessories', 'Hats'], // department
+        29.99 //price
+      ),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99)
 
-  constructor() { }
+      ];
+      
+  }
 
   ngOnInit() {
+  }
+
+ productWasSelected(product: Product): void{
+      console.log('product clicked', product);
   }
 
 }
