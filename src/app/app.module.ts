b8897a5a9af6dd3e50e3e35dd8e3b2eb002dbfa4
vinaykannambadi <vinaykannambadi@gmail.com>
2020-05-17 12:21:05 +0530

    price-department-bredcrumbs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { HelloComponent } from './hello.component';
 import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ProductComponent, ProductListComponent, ProductRowComponent ],
+  declarations: [ AppComponent, HelloComponent, ProductComponent, ProductListComponent, ProductRowComponent, ProductImageComponent, PriceDisplayComponent, ProductDepartmentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/price-display/price-display.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-price-display',
+  template: `
+ <div class="price-display">\${{ price }}</div>
+ `,
+  styleUrls: ['./price-display.component.css']
+})
+export class PriceDisplayComponent implements OnInit {
+@Input() price: number;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
